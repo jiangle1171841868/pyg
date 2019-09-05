b3abc4f092308ec55073772907c36008d3aa0637
jl <1171841868@qq.com>
2019-09-05 08:35:08 +0800

    1

---
+++ report/src/main/java/com/itheima/Test111.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: pyg
+ * @author: Mr.Jiang
+ * @create: 2019-09-03 18:57
+ * @description: 启动springBoot的入口 放在所有包的最外面
+ **/
+@SpringBootApplication
+public class Test111 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReportApplication.class, args);
+
+    }
+}
